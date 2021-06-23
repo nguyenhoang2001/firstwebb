3d94ef052891ab7810454c248bb15cd5e184e697
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2021-06-23 22:49:44 +0700

    Fourth commit

--- my_app/src/components/Pages/Home.js
+++ my_app/src/components/Pages/Home.js
@@ -1,11 +1,13 @@
 import '../../App.css';
 import HeroSection from '../HeroSection';
 import React from 'react';
+import Cards from '../Cards';
 
 function Home () {
     return (
         <>
             <HeroSection/>
+            <Cards/>
         </>
     )
 }
