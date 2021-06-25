edceca53c7df770fd9fb0506b1bd99d4f613b0da
nguyenhoang2001 <75627141+nguyenhoang2001@users.noreply.github.com>
2021-06-25 21:25:31 +0700

    5 commit

---
+++ my_app/src/components/Footer.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import {Button} from './Button'
+import './Footer.css'
+import {Link} from 'react-router-dom'
+
+function Footer() {
+    return (
+        <div className = 'footer-container'>
+            <section className='footer-subcription'>
+                <p className = 'footer-subscription-heading'>
+                    Join the Adventure newsletter to receive our best vacation deals
+                </p>
+                <p className = 'footer-subscription-text'>
+                    You can unsubscribe at any time.
+                </p>
+                <div className = 'input-areas'>
+                    <form>
+                        <input type='email' name ='email' placeholder='Your Email' className='footer-input'/>
+                        <Button buttonStyle='btn--outline'>Subscribe</Button>
+                    </form>
+                </div>
+            </section>
+            <div className='footer-links'>
+                <div className='footer-link-wrapper'>
+                    <div class='footer-link-items'>
+                        <h2>About Us</h2>
+                        <Link to='/sign-up'>How it works</Link>
+                        <Link to='/'>Testimonials</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Service</Link>
+                    </div>
+                    <div class='footer-link-items'>
+                        <h2>Contact Us</h2>
+                        <Link to='/'>Contact</Link>
+                        <Link to='/'>Support</Link>
+                        <Link to='/'>Destinations</Link>
+                        <Link to='/'>Sponsorships</Link>
+                    </div>
+                </div>
+                <div className='footer-link-wrapper'>
+                    <div class='footer-link-items'>
+                        <h2>Videos</h2>
+                        <Link to='/'>Submit Video</Link>
+                        <Link to='/'>Ambassadors</Link>
+                        <Link to='/'>Agency</Link>
+                        <Link to='/'>Influencer</Link>
+                    </div>
+                    <div class='footer-link-items'>
+                        <h2>Social Media</h2>
+                        <Link to='/'>Instagram</Link>
+                        <Link to='/'>Facebook</Link>
+                        <Link to='/'>Youtube</Link>
+                        <Link to='/'>Twitter</Link>
+                    </div>
+                </div>
+            </div>
+            <section className='social-media'>
+                <div className='social-media-wrap'>
+                    <div className='footer-logo'>
+
+                    </div>
+                </div>
+
+            </section>
+        </div>
+    )
+}
+
+export default Footer
